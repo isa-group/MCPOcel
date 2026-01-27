433ca3d99a42a020b32813a2949c4807c4826edf
Fernando Fernández <ferferga@hotmail.com>
2026-01-27 13:41:04 +0000

    refactor(gtihub2ocel): don't download schema
    The schema at the v2.0 is static, so there's no point in fetching it. It's a crucial part of the project, so it makes sense that it gets committed
    
    Signed-off-by: GitHub <noreply@github.com>

--- github2ocel/validate/validate_ocel.py
+++ github2ocel/validate/validate_ocel.py
@@ -1,42 +1,15 @@
 import json
-import os
-import requests
 from jsonschema import validate, ValidationError
 from shared.logger.logging_config import get_logger
 from pathlib import Path
 
-# Official OCEL 2.0 JSON schema URL
-SCHEMA_URL = "https://www.ocel-standard.org/2.0/ocel20-schema-json.json"
-
 logger = get_logger(__name__)
 
-def validate_ocel(ocel_path, schema_path="schemas/ocel_2_0.json"):
-
+def validate_ocel(ocel_path, schema_path=Path("schemas/ocel_2_0.json")):
     if schema_path is None:
-        # Subimos 3 niveles: validate -> github2ocel -> raíz del proyecto
-        base_dir = Path(__file__).resolve().parent.parent.parent
-        schema_path = base_dir / "schemas" / "ocel_2_0.json"
-    else:
-        schema_path = Path(schema_path)
+        raise ValueError("Schema path cannot be None")
 
     logger.info("Starting OCEL validation...")
-
-    # Official Schema Download
-    if not os.path.exists(schema_path) or os.path.getsize(schema_path) == 0:
-        os.makedirs(os.path.dirname(schema_path), exist_ok=True)
-        logger.info("Downloading official OCEL 2.0 Schema from GitHub...")
-        try:
-            resp = requests.get(SCHEMA_URL, timeout=10)
-            resp.raise_for_status()
-
-            schema_data = resp.json()
-
-            with open(schema_path, "w") as f:
-                json.dump(schema_data, f, indent=2)
-            logger.info("Schema downloaded and saved successfully.")
-        except Exception:
-            logger.exception("Failed to download the OCEL schema")
-            return False
 
     # Load files for validation
     try:
